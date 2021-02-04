b853be5720b984cb8f0aedfa236f86c0048400b2
YinX <blue.rose.hut@gmail.com>
2021-02-04 17:53:56 +0700

    abcdef

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const path = require("path");
+require("dotenv").config();
+const config = require('./config');
+const dbUtils = require('./utils/dbUtils');
+dbUtils.connectDB();
+const app = express();
+const corsOptions = {
+  origin: "*",
+  optionSuccessStatus: 200,
+};
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(cors(corsOptions));
+app.set('trust proxy', true);
+app.use(express.static(path.join(__dirname, "public")));
+global.__rootPath = path.resolve(__dirname);
+const apiVersion1 = '/api/v1';
+
+const port = process.env.PORT || 11506;
+app.listen(port, () => {
+  console.log("The app listening at %s", port);
+});
+
+app.disable("x-powered-by");
+
+require('./modules/mqttService').connect();
---
+++ modules/mqttService.js
@@ -0,0 +1,46 @@
+const mqtt = require('mqtt');
+let mqttClient;
+require("dotenv").config();
+const configs = require('../config');
+
+const Topic = {
+    companyTopic: 'companyTopic',
+    webClientTopic: 'webClientTopic',
+    deviceTopic: 'deviceTopic'
+};
+
+
+function connect() {
+    console.log("Connect with MQTT: "+configs.MqttConfig.MQTT_HOST);
+    mqttClient = mqtt.connect(configs.MqttConfig.MQTT_HOST, {
+        username: configs.MqttConfig.MQTT_USERNAME,
+        password: configs.MqttConfig.MQTT_PASSWORD
+    });
+    mqttClient.on('connect', function () {
+        console.log('Client connected to Mqtt broker');
+    });
+}
+
+function sendPubMQTT(obj = '', company, message = {}, topic = '') {
+    if (topic === Topic.companyTopic) {
+        if (['device', 'member', 'accessHistory'].includes(obj)) {
+            message.company = company;
+            message.typeName = obj;
+            mqttClient.publish(Topic.companyTopic, JSON.stringify(message));
+        }
+    } else if (topic === Topic.webClientTopic) {
+        if (['dashboard', 'device', 'uploads'].includes(obj)) {
+            message.company = company;
+            message.typeName = obj;
+            mqttClient.publish(Topic.webClientTopic, JSON.stringify(message));
+        }
+    } else if (topic === Topic.deviceTopic) {
+        message.company = company;
+        mqttClient.publish(Topic.deviceTopic, JSON.stringify(message));
+    }
+}
+
+module.exports = {
+    sendPubMQTT,
+    connect,
+};
